ad70f7d6d5a37e13fa2edcb4e92d1551ddc16b6c
Laughing <61612323+Laughing-q@users.noreply.github.com>
2020-08-26 13:47:38 +0800

    Add files via upload

---
+++ evaluate_json.py
@@ -0,0 +1,160 @@
+import logging
+import itertools
+import logging
+import os.path as osp
+import torch.distributed as dist
+
+from terminaltables import AsciiTable
+import itertools
+
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+import numpy as np
+import json
+import os
+
+import torch.utils.data as data
+
+logger_initialized = {}
+
+
+def get_logger(name, log_file=None, log_level=logging.INFO):
+    """Initialize and get a logger by name.
+
+    If the logger has not been initialized, this method will initialize the
+    logger by adding one or two handlers, otherwise the initialized logger will
+    be directly returned. During initialization, a StreamHandler will always be
+    added. If `log_file` is specified and the process rank is 0, a FileHandler
+    will also be added.
+
+    Args:
+        name (str): Logger name.
+        log_file (str | None): The log filename. If specified, a FileHandler
+            will be added to the logger.
+        log_level (int): The logger level. Note that only the process of
+            rank 0 is affected, and other processes will set the level to
+            "Error" thus be silent most of the time.
+
+    Returns:
+        logging.Logger: The expected logger.
+    """
+    logger = logging.getLogger(name)
+    if name in logger_initialized:
+        return logger
+    # handle hierarchical names
+    # e.g., logger "a" is initialized, then logger "a.b" will skip the
+    # initialization since it is a child of "a".
+    for logger_name in logger_initialized:
+        if name.startswith(logger_name):
+            return logger
+
+    stream_handler = logging.StreamHandler()
+    handlers = [stream_handler]
+
+    if dist.is_available() and dist.is_initialized():
+        rank = dist.get_rank()
+    else:
+        rank = 0
+
+    # only rank 0 will add a FileHandler
+    if rank == 0 and log_file is not None:
+        file_handler = logging.FileHandler(log_file, 'w')
+        handlers.append(file_handler)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    for handler in handlers:
+        handler.setFormatter(formatter)
+        handler.setLevel(log_level)
+        logger.addHandler(handler)
+
+    if rank == 0:
+        logger.setLevel(log_level)
+    else:
+        logger.setLevel(logging.ERROR)
+
+    logger_initialized[name] = True
+
+    return logger
+
+
+def print_log(msg, logger=None, level=logging.INFO):
+    """Print a log message.
+
+    Args:
+        msg (str): The message to be logged.
+        logger (logging.Logger | str | None): The logger to be used.
+            Some special loggers are:
+            - "silent": no message will be printed.
+            - other str: the logger obtained with `get_root_logger(logger)`.
+            - None: The `print()` method will be used to print log messages.
+        level (int): Logging level. Only available when `logger` is a Logger
+            object or "root".
+    """
+    if logger is None:
+        print(msg)
+    elif isinstance(logger, logging.Logger):
+        logger.log(level, msg)
+    elif logger == 'silent':
+        pass
+    elif isinstance(logger, str):
+        _logger = get_logger(logger)
+        _logger.log(level, msg)
+    else:
+        raise TypeError(
+            'logger should be either a logging.Logger object, str, '
+            f'"silent" or None, but got {type(logger)}')
+
+
+def evaluate(results_json, targets_json):
+    class_name = (
+        'person', 'illgal_person', 'work_cloth', 'normal_cloth', 'flower', 'tie', 'hand', 'workcard', 'phone', 'head',
+        'hand_phone')
+    # _valid_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    # cat_ids = {v: i for i, v in enumerate(_valid_ids)}
+    cocoGt = COCO(targets_json)
+    cat_ids = cocoGt.getCatIds(catNms=class_name)
+    imgIds = cocoGt.getImgIds()
+    coco_dets = cocoGt.loadRes(results_json)
+    coco_eval = COCOeval(cocoGt, coco_dets, "bbox")
+    # print(coco_eval.params.maxDets)
+    coco_eval.params.imgIds = imgIds
+    coco_eval.evaluate()
+    coco_eval.accumulate()
+    coco_eval.summarize()
+    precisions = coco_eval.eval['precision']
+    # precision: (iou, recall, cls, area range, max dets)
+    assert len(cat_ids) == precisions.shape[2]
+
+    results_per_category = []
+    for idx, catId in enumerate(cat_ids):
+        # area range index 0: all area ranges
+        # max dets index -1: typically 100 per image
+        nm = cocoGt.loadCats(catId)[0]
+        precision = precisions[:, :, idx, 0, -1]
+        precision = precision[precision > -1]
+        if precision.size:
+            ap = np.mean(precision)
+        else:
+            ap = float('nan')
+        results_per_category.append(
+            (f'{nm["name"]}', f'{float(ap):0.3f}'))
+
+    num_columns = min(6, len(results_per_category) * 2)
+    results_flatten = list(
+        itertools.chain(*results_per_category))
+    headers = ['category', 'AP'] * (num_columns // 2)
+    results_2d = itertools.zip_longest(*[
+        results_flatten[i::num_columns]
+        for i in range(num_columns)
+    ])
+    table_data = [headers]
+    table_data += [result for result in results_2d]
+    table = AsciiTable(table_data)
+    print_log('\n' + table.table, logger=None)
+    map, map50 = coco_eval.stats[:2]
+    return map
+
+
+if __name__ == '__main__':
+    evaluate('best_zw810-x_results.json', 'data/zw810/yolo_annotation/instances_val.json')
---
+++ plot_score.py
@@ -0,0 +1,123 @@
+import math
+import numpy as np
+from pathlib import Path
+import matplotlib.pyplot as plt
+import json
+import torch
+
+
+def hist2d(x, y, n=100):
+    # 2d histogram used in labels.png and evolve.png
+    xedges, yedges = np.linspace(x.min(), x.max(), n), np.linspace(y.min(), y.max(), n)
+    hist, xedges, yedges = np.histogram2d(x, y, (xedges, yedges))
+    xidx = np.clip(np.digitize(x, xedges) - 1, 0, hist.shape[0] - 1)
+    yidx = np.clip(np.digitize(y, yedges) - 1, 0, hist.shape[1] - 1)
+    return np.log(hist[xidx, yidx])
+
+
+def draw_conf_distribution(tp, conf, pred_cls, target_cls, names, save_dir):
+    # i = np.argsort(-conf)
+    # tp, conf, pred_cls = tp[i], conf[i], pred_cls[i]
+    conf = conf[tp[:, 0]]
+    pred_cls = pred_cls[tp[:, 0]]
+    # print(conf.shape)
+    # print(pred_cls.shape)
+    # Find unique classes
+    unique_classes = np.unique(target_cls)
+    conf_list = []
+    for i, c in enumerate(unique_classes):
+        conf_list.append(conf[pred_cls == c])
+    # for j in conf_list:
+    #     print(j.shape)
+    sqrt_c = math.sqrt(len(conf_list))
+    h = math.floor(sqrt_c)
+    w = math.ceil(sqrt_c)
+    fig, ax = plt.subplots(h, w, figsize=(12, 6))
+    ax = ax.ravel()
+    for i in range(len(conf_list)):
+        ax[i].scatter(conf_list[i], range(len(conf_list[i])), c=hist2d(conf_list[i], conf_list[i], 90), cmap='jet')
+        ax[i].set_title(names[int(unique_classes[i])])
+        ax[i].set_xlim(0, 1)
+    fig.tight_layout()
+    fig.savefig(Path(save_dir) / 'conf_distribution.png', dpi=200)
+    # plt.show()
+
+
+def box_iou(box1, box2):
+    def box_area(box):
+        # box = 4xn
+        return (box[2] - box[0]) * (box[3] - box[1])
+
+    area1 = box_area(box1.T)
+    area2 = box_area(box2.T)
+
+    # inter(N,M) = (rb(N,M,2) - lt(N,M,2)).clamp(0).prod(2)
+    inter = (torch.min(box1[:, None, 2:], box2[:, 2:]) - torch.max(box1[:, None, :2], box2[:, :2])).clamp(0).prod(2)
+    return inter / (area1[:, None] + area2 - inter)  # iou = inter / (area1 + area2 - inter)
+
+
+def x1y1wh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else np.zeros_like(x)
+    y[:, 0] = x[:, 0]  # top left x
+    y[:, 1] = x[:, 1]  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2]  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3]  # bottom right y
+    return y
+
+
+if __name__ == '__main__':
+    targets = json.load(open('data/zw810/yolo_annotation/instances_val.json'))  # dict
+    results = json.load(open('best_zw810-s_results.json'))  # list
+    # print(len(results))
+    # results对应targets['annotations']
+    print(results[0].keys())
+    # print(len(targets['annotations']))
+    print(targets['annotations'][0].keys())
+    print(len(targets['images']))
+    results_list = []
+    targets_list = []
+    iouv = torch.linspace(0.5, 0.95, 10)
+    niou = iouv.numel()
+    for i in range(len(targets['images'])):
+        result_i = [r for r in results if r['image_id'] == i + 1]
+        target_i = [t for t in targets['annotations'] if t['image_id'] == i + 1]
+        results_list.append(result_i)
+        targets_list.append(target_i)
+        print(result_i)
+        print(target_i)
+
+    stats = []
+    for r, t in zip(results_list, targets_list):
+        r_boxes = x1y1wh2xyxy(torch.tensor([i['bbox'] for i in r], dtype=torch.float32))
+        r_scores = torch.tensor([i['score'] for i in r], dtype=torch.float32)
+        r_preds = torch.tensor([i['category_id'] for i in r])
+        r_cls = torch.tensor([i['category_id'] - 1 for i in r])
+        # print(r_boxes.shape, r_scores.shape, r_preds.shape)
+        t_boxes = x1y1wh2xyxy(torch.tensor([i['bbox'] for i in t], dtype=torch.float32))
+        t_cls = torch.tensor([i['category_id'] - 1 for i in t])
+        # print(t_boxes.shape, t_cls.shape)
+
+        # print(ious.shape)
+        correct = torch.zeros(r_preds.shape[0], niou, dtype=torch.bool)
+        detected = []
+        nl = t_boxes.shape[0]
+        for cls in torch.unique(t_cls):
+            ti = (cls == t_cls).nonzero(as_tuple=False).view(-1)  # prediction indices
+            pi = (cls == r_cls).nonzero(as_tuple=False).view(-1)
+            if pi.shape[0]:
+                ious, i = box_iou(r_boxes[pi], t_boxes[ti]).max(1)
+                for j in (ious > iouv[0]).nonzero(as_tuple=False):
+                    d = ti[i[j]]  # detected target
+                    if d not in detected:
+                        detected.append(d)
+                        correct[pi[j]] = ious[j] > iouv  # iou_thres is 1xn
+                        if len(detected) == nl:  # all targets already located in image
+                            break
+        stats.append((correct, r_scores, r_cls, t_cls))
+    stats = [np.concatenate(x, 0) for x in zip(*stats)]
+
+    names = ['person', 'illgal_person', 'work_cloth', 'normal_cloth', 'flower', 'tie', 'hand', 'workcard', 'phone',
+             'head', 'hand_phone'
+             ]
+    draw_conf_distribution(*stats, names, save_dir='./')
