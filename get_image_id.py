065637415cd4c06e1766d65302997e6480dadf06
Laughing <61612323+Laughing-q@users.noreply.github.com>
2020-08-26 14:23:20 +0800

    Add files via upload

---
+++ get_image_id.py
@@ -0,0 +1,25 @@
+import os
+import json
+from pathlib import Path
+img_dir = './yolo_annotation'
+
+json_file = os.path.join(img_dir, "instances_val.json")
+with open(json_file) as f:
+    val_targets = json.load(f)
+
+
+record = {}
+# hw = np.loadtxt(os.path.join(img_dir, 'hw.txt'), dtype=np.long)
+for i, image in enumerate(val_targets['images']):
+    filename = Path(image['file_name']).stem
+    # height, width = cv2.imread(filename).shape[:2]
+    # width, height = Image.open(filename).size
+    idx = image['id']
+    # with open(filename.replace('jpg', 'txt').replace('png', 'txt'), 'a') as f:
+    #     f.write(str(idx))
+    record[filename] = idx
+
+print(len(record))
+f = 'image_id/val_id.json'
+with open(f, 'w') as file:
+    json.dump(record, file)
